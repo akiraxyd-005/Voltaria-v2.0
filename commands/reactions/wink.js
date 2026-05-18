65d4a4d34669c4b58255a48ac33893882e080dfa
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-18 16:30:30 +0300

    Create wink.js

---
+++ commands/reactions/wink.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+module.exports = {
+    name: 'wink',
+    category: 'reactions',
+    description: 'Send a wink GIF',
+    usage: '§wink @user',
+    async execute(sock, msg, args, extra) {
+        const mentioned = msg.message?.extendedTextMessage?.contextInfo?.mentionedJid;
+        const target = mentioned ? mentioned[0] : null;
+        
+        try {
+            const { data } = await axios.get('https://api.waifu.pics/sfw/wink');
+            const response = await axios.get(data.url, { responseType: 'arraybuffer' });
+            const buffer = Buffer.from(response.data);
+            
+            if (target) {
+                await sock.sendMessage(extra.from, {
+                    gif: buffer,
+                    caption: `@${extra.sender.split('@')[0]} winks at @${target.split('@')[0]} 😉`,
+                    mentions: [extra.sender, target]
+                }, { quoted: msg });
+            } else {
+                await sock.sendMessage(extra.from, {
+                    gif: buffer,
+                    caption: `@${extra.sender.split('@')[0]} sends a wink 😉`,
+                    mentions: [extra.sender]
+                }, { quoted: msg });
+            }
+        } catch (error) {
+            await extra.reply('❌ Failed to fetch wink GIF.');
+        }
+    }
+};
