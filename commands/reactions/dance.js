1c7df063117753c08b35fe37a158697712ce4eed
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-18 16:25:37 +0300

    Create dance.js

---
+++ commands/reactions/dance.js
@@ -0,0 +1,23 @@
+const axios = require('axios');
+
+module.exports = {
+    name: 'dance',
+    category: 'reactions',
+    description: 'Send a dance GIF',
+    usage: '§dance',
+    async execute(sock, msg, args, extra) {
+        try {
+            const { data } = await axios.get('https://api.waifu.pics/sfw/dance');
+            const response = await axios.get(data.url, { responseType: 'arraybuffer' });
+            const buffer = Buffer.from(response.data);
+            
+            await sock.sendMessage(extra.from, {
+                gif: buffer,
+                caption: `@${extra.sender.split('@')[0]} dances 💃`,
+                mentions: [extra.sender]
+            }, { quoted: msg });
+        } catch (error) {
+            await extra.reply('❌ Failed to fetch dance GIF.');
+        }
+    }
+};
