281bd7451983abe6627ad808456bdad6ef632c4b
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-20 23:53:10 +0300

    Create commands/hentai/hentaigif.js

---
+++ commands/hentai/hentaigif.js
@@ -0,0 +1,18 @@
+const hmtai = require('hmtai');
+
+module.exports = {
+    name: 'hentaigif',
+    aliases: ['hgif', 'h-gif', 'nsfwgif'],
+    category: 'hentai',
+    description: 'Get random hentai GIFs',
+    usage: '§hentaigif',
+    async execute(sock, msg, args, extra) {
+        if (!msg.isGroup) return await extra.reply('❌ NSFW commands are only available in groups.');
+        try {
+            let imageUrl = await hmtai.getNSFW('gif');
+            await sock.sendMessage(msg.chat, { video: { url: imageUrl }, gifPlayback: true, caption: '◆ *Hentai GIF*' });
+        } catch (error) {
+            await extra.reply(`❌ Error: ${error.message}`);
+        }
+    }
+};
