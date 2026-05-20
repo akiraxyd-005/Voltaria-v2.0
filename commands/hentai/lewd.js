776d20613a1fa0c59bc9baa1d57468b3af750a61
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-20 23:59:21 +0300

    Create lewd.js

---
+++ commands/hentai/lewd.js
@@ -0,0 +1,18 @@
+const hmtai = require('hmtai');
+
+module.exports = {
+    name: 'lewd',
+    aliases: ['lewdneko', 'ecchi'],
+    category: 'hentai',
+    description: 'Get random lewd images',
+    usage: '§lewd',
+    async execute(sock, msg, args, extra) {
+        if (!msg.isGroup) return await extra.reply('❌ NSFW commands are only available in groups.');
+        try {
+            let imageUrl = await hmtai.getNSFW('lewd');
+            await sock.sendMessage(msg.chat, { image: { url: imageUrl }, caption: '◆ *Lewd*' });
+        } catch (error) {
+            await extra.reply(`❌ Error: ${error.message}`);
+        }
+    }
+};
