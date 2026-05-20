7d6978fda0dbd27d3d3c0d6c32f02b035f8452eb
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-20 23:57:09 +0300

    Create cum.js

---
+++ commands/hentai/cum.js
@@ -0,0 +1,18 @@
+const hmtai = require('hmtai');
+
+module.exports = {
+    name: 'cum',
+    aliases: ['creampie', 'semen'],
+    category: 'hentai',
+    description: 'Get random cum related images',
+    usage: '§cum',
+    async execute(sock, msg, args, extra) {
+        if (!msg.isGroup) return await extra.reply('❌ NSFW commands are only available in groups.');
+        try {
+            let imageUrl = await hmtai.getNSFW('cum');
+            await sock.sendMessage(msg.chat, { image: { url: imageUrl }, caption: '◆ *Cum*' });
+        } catch (error) {
+            await extra.reply(`❌ Error: ${error.message}`);
+        }
+    }
+};
