be19bb2a3402d7475cb095334a86be71ef56a583
🌸✿𝓐𝓶𝓪𝓲-𝓬𝓱𝓪𝓷✿🌸 <akiraxyd@gmail.com>
2026-05-22 10:14:40 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,43 @@
-module.exports = {
-    prefix: process.env.PREFIX || '§',
-    botName: process.env.BOT_NAME || 'Voltaria Nexus',
-    ownerNumber: (process.env.OWNER_NUMBER || '254108720384').split(','),
-    sessionName: 'session',
-    mongodbUrl: process.env.MONGODB_URL || '',
-    timezone: 'Africa/Nairobi',
-    packName: 'Voltaria',
-    authorName: 'Nexus',
-    stickerPack: 'Voltaria Nexus',
-    apiKeys: {
-        gemini: process.env.GEMINI_API_KEY || 'YOUR_GEMINI_API_KEY',
-        openai: process.env.OPENAI_API_KEY || '',
-        weather: process.env.WEATHER_API_KEY || 'YOUR_WEATHER_API_KEY',
-        spotify: {
-            clientId: process.env.SPOTIFY_CLIENT_ID || '',
-            clientSecret: process.env.SPOTIFY_CLIENT_SECRET || ''
-        }
-    }
+const fs = require("fs");
+const { mkchar } = require("./Database/dataschema.js");
+require("./BotCharacters.js");
+
+require("dotenv").config();
+let gg = process.env.MODS;
+if (!gg) {
+  gg = "919172673114";   // You can Change this number //
+}
+
+// -------------------------------------------------------------- //
+
+
+global.owner = gg.split(",");
+global.mongodb = process.env.MONGODB || "NONE";
+global.sessionId = process.env.SESSION_ID || "ok";
+global.prefa = process.env.PREFIX || "-";
+global.tenorApiKey =
+  process.env.TENOR_API_KEY || "AIzaSyCyouca1_KKy4W_MG1xsPzuku5oa8W358c";
+global.packname = process.env.PACKNAME || `Atlas MD`;
+global.author = process.env.AUTHOR || "by: Team Atlas";
+global.port = process.env.PORT || "8000";
+
+
+
+// ---------------------Do Not Modify this part------------------- //
+
+global.mess = {
+  jobdone: "Job done...",
+  useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
+  botadmin:
+    "Sorry, i cant execute this command without being an *Admin* of this group.",
+  botowner: "Only my *Owner* can use this command, Baka!",
+  grouponly: "This command is only made for *Groups*, Baka!",
+  privateonly: "This command is only made for *Private Chat*, Baka!",
+  botonly: "Only the *Bot itself* can use this command!",
+  waiting: "Chotto Matte...",
+  nolink: "Please provide me *link*, Baka!",
+  error: "An error occurd!",
+  banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
+  bangc: "This Group is *Banned* from using Commands!",
+  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
 };
